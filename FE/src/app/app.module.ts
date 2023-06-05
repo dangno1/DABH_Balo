2caf2cdca99314930e67edeb4758f83e9f1d6103
Dang <97234801+dangno1@users.noreply.github.com>
2023-06-05 15:25:49 +0700

    ok

--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductAddComponent } from './component/product-add/product-add.component';
+import { ProductListComponent } from './component/product-list/product-list.component';
+import { ProductUpdateComponent } from './component/product-update/product-update.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    ProductListComponent,
+    ProductUpdateComponent,
+    HomePageComponent,
+    ProductDetailComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
