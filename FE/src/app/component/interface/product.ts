77e4ac072c809392f001a8a861bc5c60e3c5541e
Dang <97234801+dangno1@users.noreply.github.com>
2023-06-05 22:02:24 +0700

    updateDevelop

--- FE/src/app/component/interface/product.ts
+++
@@ -1,8 +0,0 @@
-export interface IProduct {
-    _id?:Number,
-    name:String | null | undefined,
-    price:Number | null | undefined,
-    description:String
-    category:String
-    img?:String
-}
