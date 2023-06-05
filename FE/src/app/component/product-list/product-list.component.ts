a12e3d4419631aa063cd65db1336aadf97c03e29
Minhnq24751 <mn9175719@gmail.com>
2023-06-05 21:41:33 +0700

    serviceProduct

--- FE/src/app/component/product-list/product-list.component.ts
+++ FE/src/app/component/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProduct } from '../interface/product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+ products : IProduct [] = []
 
+ constructor (private productService: ProductService){
+  this.productService.getProducts().subscribe((data)=> {
+    this.products = data
+    console.log(data);
+    
+  })
+ }
+ removeItem(id: any){
+  this.productService.deleteProduct(id).subscribe(()=> {
+    this.products = this.products.filter(item => item._id != id)
+  })
+ }
 }
--- FE/src/app/service/product.service.ts
+++ FE/src/app/service/product.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IProduct } from '../component/interface/product';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getProducts(): Observable<IProduct[]>{
+    return this.http.get<IProduct[]>('http://localhost:8088/api/show')
+  }
+  getProduct(id: number | string | null): Observable<IProduct> {
+    return this.http.get<IProduct>('http://localhost:8088/api/showProdctDetail/' + id);
+  }
+  deleteProduct(id: number | string): Observable<IProduct> {
+    return this.http.delete<IProduct>('http://localhost:8088/api/delete/' + id);
+  }
+  addProduct(product: IProduct): Observable<IProduct>{
+    return this.http.post<IProduct>('http://localhost:8088/api/add', product)
+  }
+  updateProduct(product:IProduct): Observable<IProduct>{
+    return this.http.put<IProduct>(`http://localhost:8088/api/update/${product._id}`,product)
+  }
 }
