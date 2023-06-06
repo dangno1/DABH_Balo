5a9ad77d29d48433630be76b32ac9ac2ade347ad
Dang <97234801+dangno1@users.noreply.github.com>
2023-06-06 11:12:41 +0700

    dump data from api to home

--- FE/src/app/pages/home-page/home-page.component.ts
+++ FE/src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/app/interface/product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  products: IProduct[] = [];
 
+  constructor(private productService: ProductService) {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+      // console.log(data);
+    })
+  }
 }
