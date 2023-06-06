4d08b4e05eef64eee863fd8d3049a5685749b118
Dang <97234801+dangno1@users.noreply.github.com>
2023-06-06 12:11:13 +0700

    update homePage

--- FE/src/app/app-routing.module.ts
+++ FE/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './component/product-list/product-list.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
-  {path : "admin/productList", component : ProductListComponent}
+  {path: "", component: HomePageComponent},
+  {path : "admin/productList", component : ProductListComponent},
 ];
 
 @NgModule({
