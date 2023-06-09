b08f82ce2b604f8f31ea3b41ef517e70da4c277b
Tran Viet Quang <quangtvph25239@fpt.edu.vn>
2023-06-10 01:22:21 +0700

    search by name

--- FE/src/app/pages/home-page/home-page.component.ts
+++ FE/src/app/pages/home-page/home-page.component.ts
@@ -1,19 +1,53 @@
 import { Component } from '@angular/core';
 import { IProduct } from 'src/app/interface/product';
 import { ProductService } from 'src/app/service/product.service';
-
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
+  // products: IProduct[] = [];
+  // searchTerm: string = '';
+
   products: IProduct[] = [];
+  filteredProducts: IProduct[] = [];
+  searchTerm: string = '';
 
   constructor(private productService: ProductService) {
-    this.productService.getProducts().subscribe((data) => {
-      this.products = data;
-      // console.log(data);
-    })
+    // this.productService.getProducts().subscribe((data) => {
+    //   this.products = data;
+    //   // console.log(data);
+    // });
   }
+
+  // searchProduct() {
+  //   this.productService.search(this.searchTerm).subscribe(
+  //     (products: IProduct[]) => {
+  //       this.products = products;
+  //     },
+  //     (error: any) => {
+  //       console.log(error);
+  //     }
+  //   );
+  // }
+  
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+      this.filteredProducts = products;
+    });
+  }
+
+  searchProduct(): void {
+    this.filteredProducts = this.products.filter(product =>
+      product.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  clearSearch(): void {
+    this.searchTerm = '';
+    this.filteredProducts = this.products;
+  }
+
 }
--- FE/src/app/service/product.service.ts
+++ FE/src/app/service/product.service.ts
@@ -2,27 +2,50 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IProduct } from '../interface/product';
-
+import { catchError } from 'rxjs/operators';
+import { HttpParams } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
 
-  constructor(private http: HttpClient) { }
-  getProducts(): Observable<IProduct[]>{
-    return this.http.get<IProduct[]>('http://localhost:8088/api/show')
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>('http://localhost:8088/api/show');
   }
   getProduct(id: any): Observable<IProduct> {
-    return this.http.get<IProduct>('http://localhost:8088/api/showProductDetail/' + id);
+    return this.http.get<IProduct>(
+      'http://localhost:8088/api/showProductDetail/' + id
+    );
   }
   deleteProduct(id: any): Observable<IProduct> {
     return this.http.delete<IProduct>('http://localhost:8088/api/delete/' + id);
   }
-  addProduct(product: IProduct): Observable<IProduct>{
-    return this.http.post<IProduct>('http://localhost:8088/api/add', product)
+  addProduct(product: IProduct): Observable<IProduct> {
+    return this.http.post<IProduct>('http://localhost:8088/api/add', product);
   }
-  updateProduct(product:IProduct): Observable<IProduct>{
-    return this.http.patch<IProduct>(`http://localhost:8088/api/update/${product._id}`,product)
+  updateProduct(product: IProduct): Observable<IProduct> {
+    return this.http.patch<IProduct>(
+      `http://localhost:8088/api/update/${product._id}`,
+      product
+    );
   }
+  // search(searchTerm: string): Observable<IProduct[]> {
+  //   return this.http.get<IProduct[]>(`https://localhost:8088/api/search?name=${searchTerm}`);
+  // }
+  search(term: string): Observable<IProduct[]> {
+    if (!term.trim()) {
+      // Nếu từ khóa tìm kiếm rỗng thì trả về toàn bộ sản phẩm
+      return this.getProducts();
+    }
+    const url = `https://localhost:8088/api/search?name_like=${term}`;
+    return this.http.get<IProduct[]>(url);
+  }
+  
+  
 }
